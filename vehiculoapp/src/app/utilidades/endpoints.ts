8e0579eb845009979ab6914f0672a63f0bceb9f6
htolosam <htolosam@gmail.com>
2018-05-22 19:41:42 -0500

    Version estable de angular

--- vehiculoapp/src/app/utilidades/endpoints.ts
+++
@@ -1,4 +0,0 @@
-export const ENDPOINTSUPERFICIE:string = "http://localhost:8092/api/superficie";
-export const ENDPOINTSUPERFICIEPRINCIPAL:string = "http://localhost:8092/api/superficie";
-
-export const ENDPOINTCOMANDO:string = "http://localhost:8092/com/comando";
